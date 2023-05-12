c4d292010b10da88c6aac19700f1a5c0a2fedfc7
RanaArslanShoukat <66596792+RanaArslanShoukat@users.noreply.github.com>
2023-05-12 12:38:24 +0500

    updated

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const path=require("path");
 const app=express();
 const cors=require("cors");
 dotenv.config()
+mongoose.connect(process.env.MONGO)
+.then(()=>console.log("connection successfull"))
+.then(()=>{
+     app.listen(process.env.PORT||5000);
+}).catch((err)=>console.log(err));
 
 app.use(express.json());
 app.use(cors());
@@ -16,9 +21,3 @@ app.get('*',function(req,res){
      res.sendFile(path.join(__dirname,'./client/build/index.html'))
 })
 
-mongoose.connect(process.env.MONGO)
-.then(()=>console.log("connection successfull"))
-.then(()=>{
-     app.listen(process.env.PORT||5000);
-})
-.catch((err)=>console.log(err));
